5c62147681136b44d74aaf0a4c646e90244834fe
Robert Hustead <roberthustead@MacBook-Pro-7.local>
2018-08-22 09:56:49 +0900

    Updates

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,9 +5,25 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: props.count
+      count: 0
     };
   }
+  componentDidMount() {
+    try{
+      let count = JSON.parse(localStorage.getItem('count'));
+      count = parseInt(count, 10);
+      if (!isNaN(count)){
+        this.setState(() => ({ count })); 
+      }
+    } catch(e) {
+      // Do Nothing
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
+    }
+   }
   handleAddOne() {
     // setState takes a function that returns an object
     // Usually use prevState name
@@ -44,9 +60,5 @@ class Counter extends React.Component {
     );
   }
 }
-
-Counter.defaultProps = {
-  count: 0
-};
 
 ReactDOM.render(<Counter count={4}/>, document.getElementById('app'));
